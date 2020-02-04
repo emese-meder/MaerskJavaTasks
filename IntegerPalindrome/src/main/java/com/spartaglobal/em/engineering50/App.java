279c3241dd795bfb660b8342fd1b479a868aae3e
Emese Meder <meder.emese@gmail.com>
2020-02-04 17:02:16 +0000

    Project Structure and start of coding

--- IntegerPalindrome/src/main/java/com/spartaglobal/em/engineering50/App.java
+++ IntegerPalindrome/src/main/java/com/spartaglobal/em/engineering50/App.java
@@ -1,13 +1,19 @@
 package com.spartaglobal.em.engineering50;
 
-/**
- * Hello world!
- *
- */
+/*
+Problem 6: Integer Palindrome
+        Check if given Integer is palindrome or not. An integer is called palindrome
+        if it's equal to its reverse, e.g. 1001 is a palindrome, but 1234 is not
+        because the reverse of 1234 is 4321 which is not equal to 1234.
+        Please write the unit tests for your solution using any of the unit testing framework.
+
+*/
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        IntegerPalindrome integerPalindrome = new IntegerPalindrome();
+        System.out.println(integerPalindrome.checkIntegerIsPalindrome(5) );
     }
 }
